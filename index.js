cc68ffd481dfb08164ff5f84def0ef8c030cda60
akigrp1951-source <aki.grp.1951@gmail.com>
2025-08-30 18:01:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 
-functions.http('helloWorld', async (req, res) => {
-  // L7 の到達確認用ログ（これが出ればコンテナには来ている）
+functions.http('helloWorld', (req, res) => {
   console.log('hit:', req.method, req.headers['user-agent']);
-
-  // ヘルスチェック / 誤リクエスト / 署名未実装でも、とにかく即 200 を返す
-  if (req.method !== 'POST') return res.status(200).send('OK');
-  return res.status(200).send('OK');
+  return res.status(200).send('OK'); // どんなリクエストでも 200 即返す
 });
